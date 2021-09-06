c449df9afa82993aa620f523ce80d39a0e8796a5
gisele89 <giselebartolo@gmail.com>
2021-09-06 15:07:07 -0300

    Agrega restauración de imágen original, filtro negativo y filtro de binarización

--- Entregable1/js/script.js
+++ Entregable1/js/script.js
@@ -4,7 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
     let ctx = canvas.getContext('2d');
     let width = canvas.width;
     let height = canvas.height;
+    let originalImage = null;
     let imageData = ctx.createImageData(width, height);
+
+
+
     let pencil = false;
     let rubber = false;
     let color = "black";
@@ -12,16 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
     let draw = false;
 
     //agrego los eventos al canvas para dibujar
-    canvas.addEventListener('mousedown', function(e){
+    canvas.addEventListener('mousedown', function (e) {
         startDraw(e)
     });
     canvas.addEventListener('mouseup', stopDraw);
     canvas.addEventListener('mousemove', function (e) {
         drawLine(canvas, e);
     });
-    function startDraw(e) { 
+    function startDraw(e) {
         draw = true;
-        drawLine(canvas, e);        
+        drawLine(canvas, e);
     }
 
     function stopDraw() {
@@ -104,11 +108,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     let scaledWidth = width
                     let scaledHeight = width * aspectRatio;
                     ctx.drawImage(image, 0, 0, scaledWidth, scaledHeight);
+                    originalImage = ctx.getImageData(0, 0, scaledWidth, scaledHeight);
                 }
             }
         }
     }
-    // descargo la imágen del canvas
+    // descargo la imágen del canvas usando dataToUrl asignandola a un anchor
     function downloadImage() {
         let a = document.createElement('a');
         a.href = canvas.toDataURL();
@@ -130,68 +135,109 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("#imageInput").addEventListener('change', function (e) {
         uploadImage(e);
     });
-    document.querySelector("#negativo").addEventListener('click', convertToBW);
+    document.querySelector("#byn").addEventListener('click', convertToBW);
     document.querySelector("#sepia").addEventListener('click', convertToSepia);
     document.querySelector("#brillo").addEventListener('click', addBrightnees);
     document.querySelector("#saturacion").addEventListener('click', saturation);
+    document.querySelector("#negativo").addEventListener('click', convertToNegative);
+    document.querySelector("#slider-threshold").addEventListener('click', function (e) {
+        thresholding(e);
+    })
+    document.querySelector("#original").addEventListener('click', restoreOriginalImage);
+
 
 
     //filtros de imágen
-    function convertToBW(){
-        let imageData=ctx.getImageData(0,0, canvas.width, canvas.height);
+    //en ésta función calculo un promedio que luego es asignado nuevamente a cada
+    function convertToBW() {
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-        for (let i=0;i<imageData.data.length;i+=4) {
-            let avg = (imageData.data[i]+imageData.data[i+1]+imageData.data[i+2])/3;
-        
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            let avg = (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
+
             imageData.data[i] = avg;
-            imageData.data[i+1] = avg;
-            imageData.data[i+2] = avg;
-        
+            imageData.data[i + 1] = avg;
+            imageData.data[i + 2] = avg;
+
         }
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
     }
 
-    function convertToSepia(){
-        let imageData=ctx.getImageData(0,0, canvas.width, canvas.height);
+    function convertToSepia() {
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-        for (let i=0;i<imageData.data.length;i+=4) {
-            let avg = (imageData.data[i]*0.2+imageData.data[i+1] *0.59+imageData.data[i+2]*0.11);
-        
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            let avg = (imageData.data[i] * 0.2 + imageData.data[i + 1] * 0.59 + imageData.data[i + 2] * 0.11);
+
             imageData.data[i] = avg + 100;
-            imageData.data[i+1] = avg +50;
-            imageData.data[i+2] = avg;
-        
+            imageData.data[i + 1] = avg + 50;
+            imageData.data[i + 2] = avg;
+
         }
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
     }
 
     function addBrightnees() {
-        let imageData = ctx.getImageData( 0, 0, canvas.width, canvas.height );
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
         for (let i = 0; i < imageData.data.length; i++) {
-            if ( imageData.data[i] + 10 <= 255 ) {
+            if (imageData.data[i] + 10 <= 255) {
                 imageData.data[i] = imageData.data[i] + 10;
-            }else{
+            } else {
                 imageData.data[i] = 255
             }
-            
+
         }
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
     }
 
     function saturation() {
-        let imageData = ctx.getImageData( 0, 0, canvas.width, canvas.height );
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         const r = parseInt(document.querySelector("#r-saturation-input").value);
         const g = parseInt(document.querySelector("#g-saturation-input").value);
         const b = parseInt(document.querySelector("#b-saturation-input").value);
 
-        for ( let i = 0; i< imageData.data.length; i+=4 ) {
-          imageData.data[i] = imageData.data[i] + r;
-          imageData.data[i+1] = imageData.data[i+1] + g;
-          imageData.data[i+2] = imageData.data[i+2] + b;
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            imageData.data[i] = imageData.data[i] + r;
+            imageData.data[i + 1] = imageData.data[i + 1] + g;
+            imageData.data[i + 2] = imageData.data[i + 2] + b;
         }
-        
+
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
+    }
+
+    function convertToNegative() {
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < imageData.data.length; i += 4) {
+
+            imageData.data[i] = 255 - imageData.data[i];
+            imageData.data[i + 1] = 255 - imageData.data[i + 1];
+            imageData.data[i + 2] = 255 - imageData.data[i + 2];
+
+        }
+        ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
+    }
+
+    function thresholding(e) {
+        let threshold = e.target.value;
+        console.log(threshold);
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        if (originalImage == null) {
+            originalImage = imageData;
+        } else {
+            imageData = originalImage;
+        }
+
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            let avg = (imageData.data[i] * 0.2126 + imageData.data[i + 1] * 0.7152 + imageData.data[i + 2] * 0.0722 >= threshold) ? 255 : 0;
+            imageData.data[i] = imageData.data[i + 1] = imageData.data[i + 2] = avg;
+        }
+        ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
+    }
+
+    function restoreOriginalImage() {
+        ctx.putImageData(originalImage, 0, 0);
     }
 
 })
