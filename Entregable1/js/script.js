0b20decbaf71b3a790c9cd65915a89fd3d1e5cfd
gisele89 <giselebartolo@gmail.com>
2021-09-07 16:07:42 -0300

    agrega filtro blur, correcciones de html y comentarios

--- Entregable1/js/script.js
+++ Entregable1/js/script.js
@@ -47,10 +47,10 @@ document.addEventListener("DOMContentLoaded", function () {
             ctx.stroke();
             ctx.beginPath();
             ctx.moveTo(position.x, position.y);
-        } else if (draw && rubber) {
+        } else if (draw && rubber) { 
             ctx.lineJoin = 'round';
             ctx.lineCap = 'round';
-            color = 'white';
+            color = 'white';//el valor por defecto pasa a ser blanco
             lineWidth = document.querySelector("#slider").value;
             ctx.strokeStyle = color;
             ctx.lineWidth = lineWidth;
@@ -60,7 +60,6 @@ document.addEventListener("DOMContentLoaded", function () {
             ctx.beginPath();
             ctx.moveTo(position.x, position.y);
         }
-
     }
     // obtengo coordenadas del mouse 
     function getPosition(canvas, e) {
@@ -85,7 +84,6 @@ document.addEventListener("DOMContentLoaded", function () {
             for (let y = 0; y < height; y++) {
                 setPixel(imageData, 0, 0, 255, 255, 255, 255);
             }
-
         }
         ctx.putImageData(imageData, 0, 0);
     }
@@ -144,49 +142,45 @@ document.addEventListener("DOMContentLoaded", function () {
         thresholding(e);
     })
     document.querySelector("#original").addEventListener('click', restoreOriginalImage);
+    document.querySelector("#slider-blur").addEventListener('click', blur);
 
 
 
     //filtros de imágen
-    //en ésta función calculo un promedio que luego es asignado nuevamente a cada
+    //en ésta función calculo un promedio que luego es asignado nuevamente a cada canal
     function convertToBW() {
         let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
         for (let i = 0; i < imageData.data.length; i += 4) {
             let avg = (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
-
             imageData.data[i] = avg;
             imageData.data[i + 1] = avg;
             imageData.data[i + 2] = avg;
-
         }
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
     }
 
+    //en ésta función calculo un promedio por cierto valores que luego me ayudaran a obtener la tonalidad deseada
     function convertToSepia() {
         let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
         for (let i = 0; i < imageData.data.length; i += 4) {
-            let avg = (imageData.data[i] * 0.2 + imageData.data[i + 1] * 0.59 + imageData.data[i + 2] * 0.11);
-
+            let avg = (imageData.data[i] * 0.2 + imageData.data[i + 1] * 0.59 + imageData.data[i + 2] * 0.11)
             imageData.data[i] = avg + 100;
             imageData.data[i + 1] = avg + 50;
             imageData.data[i + 2] = avg;
-
         }
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
     }
 
+    //en ésta función sumo un valor predefenido ar para agregar brillo
     function addBrightnees() {
         let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
         for (let i = 0; i < imageData.data.length; i++) {
             if (imageData.data[i] + 10 <= 255) {
                 imageData.data[i] = imageData.data[i] + 10;
             } else {
                 imageData.data[i] = 255
             }
-
         }
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
     }
@@ -196,7 +190,6 @@ document.addEventListener("DOMContentLoaded", function () {
         const r = parseInt(document.querySelector("#r-saturation-input").value);
         const g = parseInt(document.querySelector("#g-saturation-input").value);
         const b = parseInt(document.querySelector("#b-saturation-input").value);
-
         for (let i = 0; i < imageData.data.length; i += 4) {
             imageData.data[i] = imageData.data[i] + r;
             imageData.data[i + 1] = imageData.data[i + 1] + g;
@@ -206,19 +199,18 @@ document.addEventListener("DOMContentLoaded", function () {
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
     }
 
+    //en ésta función en cada canal le resto a 255 el valor que tiene en ese momento el canal para obtener el opuesto
     function convertToNegative() {
         let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
         for (let i = 0; i < imageData.data.length; i += 4) {
-
             imageData.data[i] = 255 - imageData.data[i];
             imageData.data[i + 1] = 255 - imageData.data[i + 1];
             imageData.data[i + 2] = 255 - imageData.data[i + 2];
-
         }
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
     }
 
+    //en ésta función asigno blanco o negro dependiendo de un valor umbral proporcionado por el usuario
     function thresholding(e) {
         let threshold = e.target.value;
         console.log(threshold);
@@ -235,6 +227,30 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         ctx.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
     }
+    function blur() {
+        let blur = document.querySelector("#slider-blur").value;
+        let sum = 0;
+        let delta = 5;
+        let alpha_left = 1 / (2 * Math.PI * delta * delta);
+        let step = blur < 3 ? 1 : 2;
+        for (let y = -blur; y <= blur; y += step) {
+            for (let x = -blur; x <= blur; x += step) {
+                let weight = alpha_left * Math.exp(-(x * x + y * y) / (2 * delta * delta));
+                sum += weight;
+            }
+        }
+        let count = 0;
+        for (let y = -blur; y <= blur; y += step) {
+            for (let x = -blur; x <= blur; x += step) {
+                count++;
+                ctx.globalAlpha = alpha_left * Math.exp(-(x * x + y * y) / (2 * delta * delta)) / sum * blur;
+                ctx.drawImage(canvas, x, y)
+            }
+        }
+        ctx.globalAlpha = 1;
+    }
+
+
 
     function restoreOriginalImage() {
         ctx.putImageData(originalImage, 0, 0);
