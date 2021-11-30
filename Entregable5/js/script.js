08f12f43bf1c31a634cdf24634aac11b590120e1
gisele89 <giselebartolo@gmail.com>
2021-11-30 18:16:15 -0300

    Agrega pasaje de imágenes en galeria y cambios css en chat

--- Entregable5/js/script.js
+++ Entregable5/js/script.js
@@ -73,12 +73,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     function activatePopupImage() {
         let modal = document.querySelector(".modal-photo-row");
-        let overlay = document.querySelector("#overlay");
+        let overlay = document.querySelector("#overlay-dark");
         modal.classList.remove("disable-pop-up");
         modal.classList.add("enable-pop-up");
-        overlay.classList.add("overlay");
+        overlay.classList.add("overlay-dark");
         enableChevron();
-
     }
 
     function deactivatePopupPublish() {
@@ -91,9 +90,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     function deactivatePopupImage() {
         let modal = document.querySelector(".modal-photo-row");
-        let overlay = document.querySelector("#overlay");
+        let overlay = document.querySelector("#overlay-dark");
         modal.classList.remove("enable-pop-up");
-        overlay.classList.remove("overlay");
+        overlay.classList.remove("overlay-dark");
         modal.classList.add("disable-pop-up");
     }
     function changeImageGalleryRight() {
@@ -102,6 +101,7 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (img.src.indexOf('images/gallery2-big.jpg') != -1) {
             img.src = 'images/gallery1-big.jpg';
         }
+        enableChevron();
     }
 
     function changeImageGalleryLeft() {
@@ -110,19 +110,35 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (img.src.indexOf('images/gallery1-big.jpg') != -1) {
             img.src = 'images/gallery2-big.jpg';
         }
+        enableChevron();
     }
 
-    function enableChevron() {
-        console.log("voy a habilitar");
+    function enableChevron() {        
         let chevronLeft = document.querySelector('#chevron-left');
-        let chevronRight = document.querySelector('#chevron-right');
-        if (img.src.indexOf('images/gallery2-big.jpg') != -1) {
-            chevronLeft.classList.remove("disable-pop-up");
-            chevronLeft.classList.add("enable-pop-up");
+        let chevronRight = document.querySelector('#chevron-right');        
+       
+        if (img.src.indexOf('images/gallery3-big.png') != -1) {  
+            disableChevronElement(chevronLeft);
+            enableChevronElement(chevronRight);
+        } else if (img.src.indexOf('images/gallery2-big.jpg') != -1) {
+            console.log("voy a habilitar left");
+            enableChevronElement(chevronLeft);
+            enableChevronElement(chevronRight);
+        } else if(img.src.indexOf('images/gallery1-big.jpg') != -1){
+            console.log("voy a deshabilitar right");
+            disableChevronElement(chevronRight);
+            enableChevronElement(chevronLeft);
         }
-        if(img.src.indexOf('images/gallery1-big.jpg') != -1){
-            chevronRight.classList.add("disable-pop-up");
-        }
+    }
+
+    function enableChevronElement(chevron) {
+        chevron.classList.remove("disable-pop-up");
+        chevron.classList.add("enable-pop-up");
+    }
+
+    function disableChevronElement(chevron) {
+        chevron.classList.remove("enable-pop-up");
+        chevron.classList.add("disable-pop-up");
     }
 
 
