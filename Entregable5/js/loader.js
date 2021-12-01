abf3ada376aea878b7121774099fbecffc00d8e2
lucasalmi <almiron736@gmail.com>
2021-12-01 10:44:18 -0300

    Se crea un loader

---
+++ Entregable5/js/loader.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function loaderRemove(){
+        let elementos = document.getElementsByClassName("esconder");
+        while (elementos.length > 0){
+            elementos[0].classList.remove("esconder");
+        }   
+
+        let carga = document.getElementsByClassName("centrar");
+        carga[0].classList.add("esconder");
+    }
+
+    setTimeout(loaderRemove,3000);
+});
